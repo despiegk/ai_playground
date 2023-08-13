174e43662f0480d18327320175915138a57cb326
despiegk <kristof@incubaid.com>
2023-08-13 07:17:47 +0300

    code to reproduce crash

---
+++ langchain/test.py
@@ -0,0 +1,35 @@
+import openai
+import os
+from pprint import pprint; import IPython; 
+from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
+from langchain.cache import SQLiteCache
+
+import langchain
+
+def text_to_ascii(input_text:str) -> str:
+    ascii_text = ''
+    for character in input_text:
+        ascii_text += str(ord(character)) + ' '
+    return ascii_text.rstrip()  # remove trailing space
+
+# Check if the environment variable exists
+if "OPENAIKEY" in os.environ:
+    # If it exists, get its value into a Python variable
+    api_key = os.environ["OPENAIKEY"]
+else:
+    raise ValueError("Please set the OPENAIKEY environment variable")
+
+openai.api_key = api_key
+
+
+llm = OpenAI(openai_api_key=api_key)
+chat_model = ChatOpenAI(openai_api_key=api_key)
+
+
+langchain.llm_cache = SQLiteCache(database_path=".langchain.db")
+
+llm.predict("Tell me a joke")
+llm.predict("Tell me a joke")
+
+IPython.embed()
